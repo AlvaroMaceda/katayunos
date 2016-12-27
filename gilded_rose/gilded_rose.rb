a422ce95a77f6165822b9c37ca611b3ff3ee9c59
Alvaro Maceda <alvaro@alvaromaceda.es>
2016-12-27 07:31:04 +0100

    Refactorizando condicional

--- gilded_rose/gilded_rose.rb
+++ gilded_rose/gilded_rose.rb
@@ -1,50 +1,74 @@
-def update_quality(items)
-  items.each do |item|
-    if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
-      if item.quality > 0
-        if item.name != 'Sulfuras, Hand of Ragnaros'
-          item.quality -= 1
-        end
-      end
-    else
-      if item.quality < 50
-        item.quality += 1
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          if item.sell_in < 11
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-          if item.sell_in < 6
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-        end
-      end
+
+
+def decrease_quality!(item)
+  if item.quality > 0
+    if !legendary_item?(item)
+      item.quality -= 1
     end
-    if item.name != 'Sulfuras, Hand of Ragnaros'
-      item.sell_in -= 1
-    end
-    if item.sell_in < 0
-      if item.name != "Aged Brie"
-        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          if item.quality > 0
-            if item.name != 'Sulfuras, Hand of Ragnaros'
-              item.quality -= 1
-            end
-          end
-        else
-          item.quality = item.quality - item.quality
+  end
+end
+
+def shell_date_passed?(item)
+  item.sell_in < 0
+end
+
+def backstage?(item)
+  item.name == 'Backstage passes to a TAFKAL80ETC concert'
+end
+
+def aged_brie?(item)
+  item.name == 'Aged Brie'
+end
+
+def legendary_item?(item)
+  item.name == 'Sulfuras, Hand of Ragnaros'
+end
+
+def increase_quality!(item)
+  if item.quality < 50
+    item.quality += 1
+  end
+end
+
+def update_item_quality(item)
+
+  if !aged_brie?(item) && !backstage?(item)
+    decrease_quality!(item)
+  else
+    if item.quality < 50
+      item.quality += 1
+      if backstage?(item)
+        if item.sell_in < 11
+          increase_quality!(item)
         end
-      else
-        if item.quality < 50
-          item.quality += 1
+        if item.sell_in < 6
+          increase_quality!(item)
         end
       end
     end
   end
+  if !legendary_item?(item)
+    item.sell_in -= 1
+  end
+  if shell_date_passed?(item)
+    if !aged_brie?(item)
+      if !backstage?(item)
+        decrease_quality!(item)
+      else
+        item.quality = item.quality - item.quality
+      end
+    else
+      increase_quality!(item)
+    end
+  end
 end
+
+def update_quality(items)
+  items.each do |item|
+    update_item_quality(item)
+  end
+end
+
 
 # DO NOT CHANGE THINGS BELOW -----------------------------------------
 
